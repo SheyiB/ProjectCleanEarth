6ad053e7992db0b754fd0f5ed541582186cfc8d6
Micholab <jesuwomichael@gmail.com>
2022-10-05 17:34:24 +0100

    added a new port number

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const db = require('./config/db');
-const dotenv = require('dotenv');
-const payment = require('./routes/payment.routes');
-const bodyParser = require('body-parser');
+const express = require("express");
+const db = require("./config/db");
+const dotenv = require("dotenv");
+const payment = require("./routes/payment.routes");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-dotenv.config({ path: './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 db();
 
 // routes
-app.use('/api/projectCleanEarth/pay', payment);
-
+app.use("/api/projectCleanEarth/pay", payment);
 
 // Body parser
 app.use(express.json());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server starting on port: ${port}`);
