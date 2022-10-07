6148f11ba8bfc8ca1a3d8c31e2355f77db9d6cee
denisjam400 <denisjam40@gmail.com>
2022-10-07 13:27:49 +0100

    updatedRouteNdController

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const { startPayment, createPayment, getPayment } = require('../controllers/payment');
+const express = require("express");
+const {
+  startPayment,
+  createPaymentForTransaction,
+  getPaymentFromUser,
+} = require("../controllers/payment");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/', startPayment );
-router.get('/createPayment', createPayment);
-router.get('/paymentDetails', getPayment);
+router.post("/", startPayment);
+router.get("/createPayment", createPaymentForTransaction);
+router.get("/paymentDetails", getPaymentFromUser);
 
 module.exports = router;
